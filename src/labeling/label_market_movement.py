a88521865fe7cc00aaa34eb68962869922b98fbd
bosungsuk08 <bosungsuk08@gmail.com>
2025-04-29 18:19:31 +0900

    bugs fixed

--- src/labeling/label_market_movement.py
+++ src/labeling/label_market_movement.py
@@ -1,14 +1,21 @@
 from src.preprocessing.sentiment_utils import get_sentiment_score
+from datetime import datetime
 import pandas as pd
 
-def label_market_movement(news_title: str, news_date: str, stock_data: pd.DataFrame):
+#new
+def label_market_movement(news_title: str, news_date: pd.Timestamp, stock_data: pd.DataFrame):
     
     sentiment = get_sentiment_score(news_title)
     compound_score = sentiment['compound']
+    
+    news_date = news_date.normalize()
+    print(news_date)
 
     current_close = stock_data.loc[stock_data['Date'] == news_date, 'Close'].values
     next_day_close = stock_data.loc[stock_data['Date'] > news_date, 'Close'].head(1).values
 
+    print('current', current_close)
+    print('next', next_day_close)
     if len(current_close) == 0 or len(next_day_close) == 0:
         return None 
 
@@ -26,7 +33,7 @@ def label_market_movement(news_title: str, news_date: str, stock_data: pd.DataFr
         label = 0  
 
     return {
-        "text": news_title,
+        "news_title": news_title,
         "sentiment_score": compound_score,
         "return_rate": return_rate,
         "label": label
@@ -34,11 +41,13 @@ def label_market_movement(news_title: str, news_date: str, stock_data: pd.DataFr
 
 def label_multiple_news(news_df: pd.DataFrame, stock_data: pd.DataFrame):
 
-    labeled_data = []
+    #convert the news_df['date'] to a naive datetime (without timezone)
+    news_df['date'] = news_df['date'].dt.tz_localize(None)
 
+    labeled_data = []
     for _, row in news_df.iterrows():
         result = label_market_movement(
-            news_text=row['text'],
+            news_title=row['title'],
             stock_data=stock_data,
             news_date=row['date']
         )
