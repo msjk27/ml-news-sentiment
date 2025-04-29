e7a4b1e38b0acd7ee24bf0e6d6f6cec8f789542a
bosungsuk08 <bosungsuk08@gmail.com>
2025-04-29 18:17:31 +0900

    get current date + 10 days stock data

--- src/data/fetch_stock_data.py
+++ src/data/fetch_stock_data.py
@@ -3,6 +3,13 @@ import pandas as pd
 
 
 def fetch_stock_data(stock_name:str, date:str):
+    
+    start_date = pd.to_datetime(date)
 
-    return (yf.download(stock_name, start= date, end= date))
+    end_date = end_date = start_date + pd.Timedelta(days=10)
+
+    #yf.download는 column 을 index 로 설정함. '.rest_index()' 로 column 으로 바꿔줌
+    stock_data = (yf.download(stock_name, start= start_date, end= end_date)).reset_index()
+
+    return (stock_data)
 
